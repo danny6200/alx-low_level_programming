b0ba1d2a1f22d630629fab58fa55a84ef22dd881
danny6200 <ugookorodaniel620@gmail.com>
2023-06-15 13:00:21 -0700

    Add: comments to 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
+/**
+ * main - This function puts some string to stdout
+ * Return - 0 if the function executes successfully
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
-
