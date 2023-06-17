00d1cfb9bab268c63a42fc8d7a78cfc1afdc9532
danny6200 <ugookorodaniel620@gmail.com>
2023-06-17 15:47:28 -0700

    Add: 101-quote c and output files

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - prints some text to standard output
+ *
+ * Return: 1 after executing successfully
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
