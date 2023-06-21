58b7707e5bd60f0bde15b52cc228d38ddbdf6bab
danny6200 <ugookorodaniel620@gmail.com>
2023-06-21 09:33:40 -0700

    Add: 2-print_alphabet_x10.c files

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints letter of the alphabets in lower
+ * case, ten times.
+ *
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+	return;
+}
