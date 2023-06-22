cab87c5f8546710dde13a0ffbec061e194c68121
danny6200 <ugookorodaniel620@gmail.com>
2023-06-22 14:16:02 -0700

    Add: modified main.h file and 10-print_triangle files

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle with base of size n
+ * @size: determines the size of the triangle's base
+ *
+ * Return: nothing
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = i; j < size; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 0; k < i; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
