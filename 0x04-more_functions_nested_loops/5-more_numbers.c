5e01797710fbdf3d0205b4239aa50f0365f8b52e
danny6200 <ugookorodaniel620@gmail.com>
2023-06-22 10:57:33 -0700

    Add: modified main.h file and 5-more_numbers files

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers from 0 to 14
+ *
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int c;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (c = 0; c < 15; c++)
+		{
+			if (c > 9)
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+			else
+			{
+				_putchar(c + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
