79201b2bdae522e69a54d93ae9e2f3f7883b6894
danny6200 <ugookorodaniel620@gmail.com>
2023-06-22 09:38:15 -0700

    Add: modified main.h and 2-mul files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - returns the result for the mulplication of two integers
+ * @a: integer to be multiplied
+ * @b: integer to be multiplied
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int res = a * b;
+
+	return (res);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,3 +10,4 @@ void jack_bauer(void);
 void times_table(void);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
