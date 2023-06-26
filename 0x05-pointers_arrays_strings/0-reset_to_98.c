fa3f8c70416763fa8ea228c43a9aa6270f886ee0
danny6200 <ugookorodaniel620@gmail.com>
2023-06-26 14:35:26 -0700

    Add: README.md, main.h and 0-reset_to_98.c files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - changes any integer passed to it to 98
+ * @n: points to the memory location of integer passed
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
