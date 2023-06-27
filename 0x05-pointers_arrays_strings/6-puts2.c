1ecfea9065216e620afb2904df314ebbe3a7a101
danny6200 <ugookorodaniel620@gmail.com>
2023-06-26 17:28:25 -0700

    Add: main.h file and 6-puts2 files

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string starting from the first.
+ * @str: string whose other charcter is to be printed
+ *
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int length = 0;
+	int i;
+
+	while (str[length] != 0)
+		length++;
+
+	for (i = 0; i < length - 1; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
