ffc9fa8f3ead1ddbfafb32004ab869abfea52236
danny6200 <ugookorodaniel620@gmail.com>
2023-06-26 17:12:04 -0700

    Add: main.h file and 5-rev_string files

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - reverses a given string
+ * @s: string to be reversed
+ *
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	int m, n;
+
+	while (s[length] != 0)
+		length++;
+
+	for (m = 0, n = length - 1; m < n; m++, n--)
+	{
+		char temp = s[m];
+		s[m] = s[n];
+		s[n] = temp;
+	}
+}
+
