76790da53065de50a0ee2d5bcf84155e7355078b
danny6200 <ugookorodaniel620@gmail.com>
2023-06-29 14:28:30 -0700

    Add: README.md, main.h and 0-strcat files

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: points to string 1, the destination for the concatenation
+ * @src: points to string 2, to be concat with string 1
+ *
+ *
+ * Return: p, points to dest, which holds the concat of the strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+
+	while (*src != '\0')
+	{
+		*p = *src;
+		p++;
+		src++;
+	}
+
+	*p = '\0';
+
+	return (p);
+}
+
