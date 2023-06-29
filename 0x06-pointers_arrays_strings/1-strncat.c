3ee8d5c66830438b66296a34f2ca7d16cd91c620
danny6200 <ugookorodaniel620@gmail.com>
2023-06-29 15:17:37 -0700

    Add: main.h and 1-strncat files

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strncat - concats two strings up to n character of str2
+ * @dest:holds destination address for concat result
+ * @src: holds address for str 2
+ *
+ * Return: p, pointer to pointer dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *p = dest;
+	char *dest_ptr = p;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+
+	while (*src != '\0' && n > 0)
+	{
+		*p = *src;
+		p++;
+		src++;
+		n--;
+	}
+
+	*p = '\0';
+
+	return (dest_ptr);
+
+}
+
