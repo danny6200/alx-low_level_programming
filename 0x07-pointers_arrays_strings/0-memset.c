4c057ccc23c10ba89705126da2ed6d2f6dd0a2d0
danny6200 <ugookorodaniel620@gmail.com>
2023-07-03 10:45:49 -0700

    README.md, main.h and 0-memset files

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+/**
+ * _memset - fills a memory area with byte b up to n bytes
+ * @s: points to the starting memory address
+ * @b: byte to be used in filling the memory area
+ * @n: determines the limit of the memory area to be filled
+ *
+ * Return: pointer to a pointer to a char
+ */
+#include "main.h"
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	char **p;
+
+	p = &s;
+	i = 0;
+
+	while (i < n)
+	{
+		*s = b;
+		s++;
+		i++;
+	}
+	return (*p);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This project is still about pointers, strings and arrays.
