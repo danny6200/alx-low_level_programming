9f28346b5704c2d34525b9dbb1d185a2a62282a7
danny6200 <ugookorodaniel620@gmail.com>
2023-07-03 11:23:41 -0700

    Add: main.h, 1-memcpy files and 1-main.c file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+/**
+ * _memcpy - copies n bytes from a source memory area
+ * to a destination's memory area
+ * @dest: pointer to the destination memory area
+ * @src: pointer to the source memory area
+ * @n: number of bytes to be copied
+ *
+ * Return: pointer to a pointer to a character
+ */
+#include "main.h"
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+	char **p = &dest;
+
+	while (i < n)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		i++;
+	}
+	return (*p);
+}
