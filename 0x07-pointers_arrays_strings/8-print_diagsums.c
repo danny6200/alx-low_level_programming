30d281bae512f5c2b9afaec42bc7ce144931d1fc
danny6200 <ugookorodaniel620@gmail.com>
2023-07-04 07:56:09 -0700

    Add main.h file and 8-print_diagsums file

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+/**
+ * print_diagsums - adds up all numbers along the diagonals
+ * @a: pointer to array
+ * @size: gives the size of the array
+ *
+ * Return: nothing
+ */
+#include "main.h"
+#include <stdio.h>
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int lsum = 0;
+	int rsum = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		rsum += a[i*size + i];
+		lsum += a[i*size + size - i - 1] ;
+	}
+	printf("%d, %d\n", rsum, lsum);
+}
