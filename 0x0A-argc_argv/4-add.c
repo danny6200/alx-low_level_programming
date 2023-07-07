fb84e78185a83afa6e329dd110ce4804b94195f7
danny6200 <ugookorodaniel620@gmail.com>
2023-07-07 16:29:21 -0700

    Add 4-add.c and .gitignore file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+/**
+ * main - adds numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc == 1)
+		printf("0\n");
+	for (i = 1; i < argc; i++)
+	{
+		char *numstr = argv[i];
+		int j;
+
+		for (j = 0; numstr[j] != '\0'; j++)
+		{
+			if (!isdigit(numstr[j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
