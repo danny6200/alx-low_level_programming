b71e0ff5668dfb919a0869d4085489e3bcacc7ef
danny6200 <ugookorodaniel620@gmail.com>
2023-07-11 15:06:42 -0700

    Add 1-strdup, main.h, and .gitignore file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+/**
+ * _strdup - duplicates a string in a newly allocated memory
+ * @str: pointer to the string to be copied
+ *
+ * Return: pointer to the string's duplicate
+ */
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+char *_strdup(char *str)
+{
+	int i, len = 0;
+	char *s = malloc(len * sizeof(char));
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		s[i] = str[i];
+
+	return (s);
+}
+
