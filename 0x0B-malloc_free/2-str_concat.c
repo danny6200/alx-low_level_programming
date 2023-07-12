90d57cfce080802ea7e7586f56b630eac997cc69
danny6200 <ugookorodaniel620@gmail.com>
2023-07-12 10:32:26 -0700

    Modify 2-str_concat again again

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,7 +14,7 @@ char *str_concat(char *s1, char *s2)
 	int len1 = 0;
 	int len2 = 0;
 	int i, j;
-	char *concat = malloc((len1 + len2 + 1) * sizeof(char));
+	char *concat;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -24,6 +24,9 @@ char *str_concat(char *s1, char *s2)
 		len1++;
 	while (s2[len2] != '\0')
 		len2++;
+
+	concat = malloc((len1 + len2 + 1) * sizeof(char));
+
 	if (concat == NULL)
 		return (NULL);
 
