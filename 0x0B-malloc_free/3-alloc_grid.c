a6edcf17d8e6e8ac84a6ca984fed3caa6f8af15b
danny6200 <ugookorodaniel620@gmail.com>
2023-07-12 13:42:28 -0700

    Modify 3-alloc_grid.c'

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,20 +16,22 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	grid = malloc(height * sizeof(int *));
+	grid = (int **)malloc(height * sizeof(int *));
 	if (grid == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
-		grid[i] = malloc(width * sizeof(int));
-	if (grid[i] == NULL)
 	{
-		for (k = 0; k < i; k++)
-			free(grid[k]);
-		free(grid);
-		return (NULL);
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (k = 0; k < i; k++)
+				free(grid[k]);
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
 	}
-	for (j = 0; j < width; j++)
-		grid[i][j] = 0;
 	return (grid);
 }
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,4 @@
+/**
+ * free_grid - frees a memory space where a two dimensuonal array
+ * lives
+ * @grid: two dimensional array
