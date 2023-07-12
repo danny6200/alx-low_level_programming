aa822c59ad45720620064f48befb5d88d87fec31
danny6200 <ugookorodaniel620@gmail.com>
2023-07-12 10:45:59 -0700

    Modify 1-strdup.c again again

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,13 +18,13 @@ char *_strdup(char *str)
 	while (str[len] != '\0')
 		len++;
 
-	s = malloc(len * sizeof(char));
+	s = malloc((len + 1) * sizeof(char));
 
 	if (s == NULL)
 		return (NULL);
 
 	for (i = 0; i < len; i++)
 		s[i] = str[i];
-
+	s[i] = '\0';
 	return (s);
 }
