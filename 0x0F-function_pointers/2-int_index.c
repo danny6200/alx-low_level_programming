fe6d5f518cc56d5626d7fb601265e1ebaccc8bc9
danny6200 <ugookorodaniel620@gmail.com>
2023-07-19 09:04:36 -0700

    Add 2-int_index.c and modify function_pointers.h

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+/**
+ * int_index - returns the index of an element that matches a given integer
+ * @array: array to search for the given integer
+ * @size: size of the given array
+ * @cmp: pointer to the compare function
+ *
+ * Return: the index of the found integer on success
+ */
+#include <stddef.h>
+#include "function_pointers.h"
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]))
+			return (i);
+	}
+	return (-1);
+}
