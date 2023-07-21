6f8dc0d58b314d023f8beafc6d4c37d2a36a7ddd
danny6200 <ugookorodaniel620@gmail.com>
2023-07-21 03:40:23 -0700

    Modify 2-print_strings.c again

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list ap;
 
 	if (n == 0)
+	{
+		printf("\n");
 		return;
+	}
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
