2cb37530378710c29bcf456aa380d1619c0123ab
danny6200 <ugookorodaniel620@gmail.com>
2023-08-01 10:42:17 -0700

    Modify 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,15 +14,16 @@ size_t print_listint(const listint_t *h)
 	size_t count = 0;
 	listint_t *tmp;
 
-	tmp = (listint_t *)h;
-
 	if (h == NULL)
 		return (1);
+	tmp = (listint_t *)h;
+
 	while (tmp != NULL)
 	{
 		printf("%d\n", tmp->n);
 		count++;
 		tmp = tmp->next;
 	}
+	free(tmp);
 	return (count);
 }
