fe1cefef663e4020038b50112cfb34e3851a1f51
danny6200 <ugookorodaniel620@gmail.com>
2023-08-07 17:52:00 -0700

    Add files for task 1 and task 2

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+/**
+ * create_file - creates a file with som text if made available
+ * @filename: name of file
+ * @text_content: content to be written to file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int create_file(const char *filename, char *text_content)
+{
+	int bytes_written, fd;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		perror("Error in opening file");
+		close(fd);
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			perror("Error writing to file");
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
