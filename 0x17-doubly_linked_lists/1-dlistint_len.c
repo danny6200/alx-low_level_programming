0c4da468f3eef88e67274249bff63ed075bca299
danny6200 <ugookorodaniel620@gmail.com>
2023-09-15 09:24:53 -0700

    Add: task 1 file

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+/**
+ * dlistint_len - returns the number of nodes in a dll
+ * @h: points to the head node
+ *
+ * Returns: number of nodes
+ */
+#include "lists.h"
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	dlistint_t *temp = (dlistint_t *)h;
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
