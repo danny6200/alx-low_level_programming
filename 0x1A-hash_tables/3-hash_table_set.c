69863a2674df5dd731232e8358bbf11af3091f9e
danny6200 <ugookorodaniel620@gmail.com>
2023-10-06 23:30:35 +0100

    Modify: 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,40 @@
 /**
+ * create_hash_node - creates an hash node
+ * @key: key of value to be stored
+ * @value: value to be stored
+ *
+ * Return: pointer to the hash_node or NULL
+*/
+#include "hash_tables.h"
+hash_node_t *create_hash_node(const char *key, const char *value)
+{
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+
+	if (!new_node)
+	{
+		return (NULL);
+	}
+
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->value = strdup(value);
+	if (!new_node->value)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/**
  * hash_table_set - adds an element to the hash table
  * @ht: pointer to the hash table
  * @key: key of the value to be stored
@@ -6,7 +42,7 @@
  *
  * Return: 1 on success, 0 otherwise
 */
-#include "hash_tables.h"
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
@@ -30,8 +66,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
 	}
-	free(new_node->key);
-	free(new_node->value);
-	free(new_node);
 	return (1);
 }
